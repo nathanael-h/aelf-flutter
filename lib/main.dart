d151c28a1cdb4095b576e5383ddcb00c8f980dff
Nathanael <nathanael@hannebert.com>
2019-08-11 23:10:40 +0200

    Remove code for counter from default flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -46,8 +46,6 @@ class MyApp extends StatelessWidget {
         // changing the primarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.red,
       ),
       home: MyHomePage(storage: CounterStorage()),
@@ -63,28 +61,9 @@ class CounterStorage {
     return directory.path;
   }
 
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    return File('$path/counter.txt');
-  }
-
   Future<File> get _localChapter async {
     final path = await _localPath;
     return File('$path/assets/chapter.txt');
-  }
-
-  Future<int> readCounter() async {
-    try {
-      final file = await _localFile;
-
-      // Read the file
-      String contents = await file.readAsString();
-
-      return int.parse(contents);
-    } catch (e) {
-      // If encountering an error, return 0
-      return 0;
-    }
   }
 
   Future<String> readChapter() async {
@@ -104,12 +83,6 @@ class CounterStorage {
     return await rootBundle.loadString('assets/chapter.txt');
   }
 
-  Future<File> writeCounter(int counter) async {
-    final file = await _localFile;
-
-    // Write the file
-    return file.writeAsString('$counter');
-  }
 }
 
 
@@ -131,8 +104,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //int _counter = 0;
-  int _counter;
+
   String chapter;
   List listNewTestamentBooks = [
       "Évangile selon Saint Matthieu",
@@ -167,12 +139,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    // Read the value in the file, and set it to the variable _counter
-    widget.storage.readCounter().then((int value) {
-      setState(() {
-        _counter = value;
-      });
-    });
     widget.storage.loadAsset().then((String text) {
       setState(() {
         chapter = text;
@@ -180,23 +146,10 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  Future<File> _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-    // Write the variable as a string to the file.
-    return widget.storage.writeCounter(_counter);
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // This method is rerun every time setState is called.
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
@@ -290,17 +243,6 @@ class _MyHomePageState extends State<MyHomePage> {
                     ListTile(
                       title: Text('Livre 1'),
                     ),
-                    ListTile(
-                      title: Text(
-                        'You have pushed the button this many times:',
-                      ),
-                    ),
-                    ListTile(
-                      title: Text(
-                        '$_counter',
-                        style: Theme.of(context).textTheme.display1,
-                      ),
-                    ),
                   ],
                 ),
               ),
@@ -347,11 +289,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
