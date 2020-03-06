be876e3f1ecdb648a0e598d05ffbdcd41eebc97d
Nathanael <nathanael@hannebert.com>
2020-03-06 20:28:42 +0100

    Global: add linkify to turn URLs in text into clickable inline links in text

--- lib/app_screens/not_dev_screen.dart
+++ lib/app_screens/not_dev_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_linkify/flutter_linkify.dart';
 class ToDo {
   String feature;
 
@@ -9,7 +9,10 @@ class ToDo {
     print("Cette fonctionnalité (\"$feature\") n'est pas encore développée. \nCette application est libre et open source, si vous souhaitez nous aider à contribuer c'est possible sur :\ngitlab.com/nathanael2/aelf-flutter ");
     var popUp = AlertDialog(
       title: Center(child: Text("🚧 App en chantier 🛠")),
-      content: Text("Cette fonctionnalité (\"$feature\") n'est pas encore développée. Cette application est libre🆓 et open source🔓, et le développement 👨‍💻 est encore en cours🚧. Toute aide est la bienvenue ! 🙂 🎉 💪 Voyez cette page :\n👉🏼gitlab.com/nathanael2/aelf-flutter "),
+      content: SingleChildScrollView(child: Linkify(
+        onOpen: (link) => print("Clicked ${link.url}"),
+        text: "Cette fonctionnalité (\"$feature\") n'est pas encore développée. Cette application est libre🆓 et open source🔓, et le développement 👨‍💻 est encore en cours🚧. Toute aide est la bienvenue ! 🙂 🎉 💪 Voyez cette page :\n👉🏼 https://gitlab.com/nathanael2/aelf-flutter "
+        )),
     );
     showDialog(context: context, builder: (BuildContext contect) => popUp);
   }
