05e686688e4afd504d7aef5de72b8f376139c172
Nathanael <nathanael@hannebert.com>
2020-05-01 23:04:31 +0200

    About: links open browser on click

--- lib/app_screens/about_screen.dart
+++ lib/app_screens/about_screen.dart
@@ -1,21 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class About {
   void popUp(BuildContext context) {
     var popUp = AlertDialog(
       title: Center(child: Text("À propos")),
       content: SingleChildScrollView(
-        child: Linkify(
-            onOpen: (link) => print("Clicked ${link.url}"),
-            text:
-                ("L'application AELF est développée bénévolement par quelques volontaires. Actuellement elle vous permet d'avoir sur votre iPhone toute la Bible dans la traduction française liturgique. Cette traduction est le fruit du travail de l'AELF, l'Association épisccopale liturgique pour les pays francophones. \n \n Cette application est libre🆓 et open source🔓, et le développement 👨‍💻 est encore en cours🚧. Toute aide est la bienvenue ! 🙂 🎉 💪 Pour toute question, remarque ou proposition d'aide, voyez cette page :\n👉🏼 https://gitlab.com/nathanael2/aelf-flutter ")),
+        child: Column(
+          children: <Widget>[
+            Linkify(
+                onOpen: (link) => _launchInBrowser(link.url),
+                text:
+                    ("L'application AELF est développée bénévolement par quelques volontaires. Actuellement elle vous permet d'avoir sur votre iPhone toute la Bible dans la traduction française liturgique. Cette traduction est le fruit du travail de l'AELF, l'Association épisccopale liturgique pour les pays francophones. Voir le site pour plus d'informations : https://www.aelf.org/page/les-missions-de-laelf \n \n Cette application est libre🆓 et open source🔓, et le développement 👨‍💻 est encore en cours🚧. Toute aide est la bienvenue ! 🙂 🎉 💪 Pour toute question, remarque ou proposition d'aide, voyez cette page :\n👉🏼 https://gitlab.com/nathanael2/aelf-flutter ")),
+          ],
+        ),
       ),
     );
     showDialog(context: context, builder: (BuildContext contect) => popUp);
   }
 }
-// TODO : make link clickable
+
+Future<void> _launchInBrowser(String url) async {
+  if (await canLaunch(url)) {
+    await launch(
+      url,
+      forceSafariVC: false,
+      forceWebView: false,
+    );
+  } else {
+    throw 'Could not launch $url';
+  }
+}
 // TODO : add a "Valider" button
 // TODO : add a changelog section
 // TODO : jusitfy text
