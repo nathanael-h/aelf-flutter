313482224f4c54297949b2ae063c2e4c4c68c134
Nathanael <nathanael@hannebert.com>
2020-05-01 23:19:48 +0200

    About: justify text and add OK button

--- lib/app_screens/about_screen.dart
+++ lib/app_screens/about_screen.dart
@@ -10,9 +10,18 @@ class About {
         child: Column(
           children: <Widget>[
             Linkify(
+                textAlign: TextAlign.justify,
                 onOpen: (link) => _launchInBrowser(link.url),
                 text:
                     ("L'application AELF est développée bénévolement par quelques volontaires. Actuellement elle vous permet d'avoir sur votre iPhone toute la Bible dans la traduction française liturgique. Cette traduction est le fruit du travail de l'AELF, l'Association épisccopale liturgique pour les pays francophones. Voir le site pour plus d'informations : https://www.aelf.org/page/les-missions-de-laelf \n \n Cette application est libre🆓 et open source🔓, et le développement 👨‍💻 est encore en cours🚧. Toute aide est la bienvenue ! 🙂 🎉 💪 Pour toute question, remarque ou proposition d'aide, voyez cette page :\n👉🏼 https://gitlab.com/nathanael2/aelf-flutter ")),
+            Align(
+              alignment: Alignment.centerRight,
+              child: FlatButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('Valider'),
+                textColor: Colors.black,
+              ),
+            )
           ],
         ),
       ),
@@ -34,4 +43,3 @@ Future<void> _launchInBrowser(String url) async {
 }
 // TODO : add a "Valider" button
 // TODO : add a changelog section
-// TODO : jusitfy text
