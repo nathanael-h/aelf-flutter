ddd38bd85599b871615dbc5cc67c398070b607dc
fred <fedwiiix@gmail.com>
2020-05-10 19:05:25 +0200

    add bible db helper

---
+++ lib/bibleDbHelper.dart
@@ -0,0 +1,164 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
+
+class BibleDbHelper {
+  // define all db parameters
+  static final _databaseName = "bible.db";
+
+  // make this a singleton class
+  BibleDbHelper._privateConstructor();
+  static final BibleDbHelper instance = BibleDbHelper._privateConstructor();
+
+  // only have a single app-wide reference to the database
+  static Database _database;
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    // lazily instantiate the db the first time it is accessed
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  // this opens the database (and creates it if it doesn't exist)
+  _initDatabase() async {
+    var databasesPath = await getDatabasesPath();
+    var path = join(databasesPath, _databaseName);
+    //await deleteDatabase(path);
+    // Check if the database exists
+    var exists = await databaseExists(path);
+    if (!exists) {
+      // Should happen only the first time you launch your application
+      print("Creating new copy from asset");
+      // Make sure the parent directory exists
+      try {
+        await Directory(dirname(path)).create(recursive: true);
+      } catch (_) {}
+      // Copy from asset
+      ByteData data = await rootBundle.load(join("assets", _databaseName));
+      List<int> bytes =
+      data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      // Write and flush the bytes written
+      await File(path).writeAsBytes(bytes, flush: true);
+    } else {
+      print("Opening existing database");
+    }
+    // open the database
+    return await openDatabase(path, readOnly: true);
+  }
+
+  // Helper methods
+
+  // get chapter
+  Future<Chapter> getChapter(String book, String chapter) async {
+    Database db = await instance.database;
+    dynamic results = await db.rawQuery(
+        'SELECT * FROM chapters WHERE book=? AND chapter=? LIMIT 1',
+        [book, chapter]);
+
+    if (results.length > 0) {
+      return new Chapter.fromMap(results.first);
+    }
+    return null;
+  }
+
+  // get chapter
+  Future<Verse> getVerse(String book, String chapter, String verse) async {
+    Database db = await instance.database;
+    dynamic results = await db.rawQuery(
+        'SELECT * FROM verses WHERE book=? AND chapter=? AND verse=? LIMIT 1',
+        [book, chapter, verse]);
+
+    if (results.length > 0) {
+      return new Verse.fromMap(results.first);
+    }
+    return null;
+  }
+
+}
+/* call examples
+import 'package:aelf_flutter/bibleDbHelper.dart';
+BibleDbHelper bibleDbHelper = BibleDbHelper.instance;
+bibleDbHelper.getChapter("Gn", "1").then((Chapter chapter){
+  print(chapter.text);
+});
+bibleDbHelper.getVerse("Jn", "8", "32").then((Verse verse){
+  print(verse.text);
+});*/
+
+class Chapter {
+  Chapter({
+    this.book,
+    this.book_id,
+    this.chapter,
+    this.chapter_id,
+    this.title,
+    this.text,
+  });
+
+  factory Chapter.fromMap(Map<String, dynamic> data) => new Chapter(
+    book: data["book"],
+    book_id: data["book_id"],
+    chapter: data["chapter"],
+    chapter_id: data["chapter_id"],
+    title: data["title"],
+    text: data["text"],
+  );
+
+  String book;
+  int book_id;
+  String chapter;
+  int chapter_id;
+  String title;
+  String text;
+
+  Map<String, dynamic> toMap() => {
+    "book": book,
+    "book_id": book_id,
+    "chapter": chapter,
+    "chapter_id": chapter_id,
+    "title": title,
+    "text": text,
+  };
+}
+
+class Verse {
+  Verse({
+    this.book,
+    this.book_id,
+    this.book_title,
+    this.chapter,
+    this.chapter_id,
+    this.verse,
+    this.text,
+  });
+
+  factory Verse.fromMap(Map<String, dynamic> data) => new Verse(
+    book: data["book"],
+    book_id: data["book_id"],
+    book_title: data["book_title"],
+    chapter: data["chapter"],
+    chapter_id: data["chapter_id"],
+    verse: data["verse"],
+    text: data["text"],
+  );
+
+  String book;
+  int book_id;
+  String book_title;
+  String chapter;
+  int chapter_id;
+  int verse;
+  String text;
+
+  Map<String, dynamic> toMap() => {
+    "book": book,
+    "book_id": book_id,
+    "book_title": book_title,
+    "chapter": chapter,
+    "chapter_id": chapter_id,
+    "verse": verse,
+    "text": text,
+  };
+}
