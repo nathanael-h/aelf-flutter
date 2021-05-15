f71b7c6448043e2d5a38561c725d5f9627ddfbbf
Nathanael <nathanael@hannebert.com>
2021-05-15 22:37:41 +0200

    Remove unused import

--- lib/app_screens/liturgy_formatter.dart
+++ lib/app_screens/liturgy_formatter.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
-import 'package:html/dom.dart' as dom;
 
 
 class LiturgyFormatter extends StatefulWidget {
