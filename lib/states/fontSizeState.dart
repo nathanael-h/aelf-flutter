587d0c85c856ea9cc76d50c5ce95aed304e7c2a9
Nathanael <nathanael@hannebert.com>
2022-06-22 08:43:18 +0200

    add: fontSizeState.dart

---
+++ lib/states/fontSizeState.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FontSizeNotifier extends ChangeNotifier {
+final String keyFontSize = 'keyFontSize';
+  SharedPreferences _pref;
+  double _fontSize;
+
+  double get fontSize => _fontSize;
+
+  FontSizeNotifier() {
+    _fontSize = 14.0;
+    _loadFromPrefs();
+  }
+
+  udateFontSize(double fontSize){
+    _fontSize = this.fontSize;
+    _saveToPrefs();
+    notifyListeners();
+  }
+
+ // _initPref() is to iniliaze  the _pref variable
+  _initPrefs() async {
+    if(_pref == null)
+      _pref  = await SharedPreferences.getInstance();
+  }
+  _loadFromPrefs() async {
+      await _initPrefs();
+      _fontSize = _pref.getDouble(keyFontSize) ?? 14.0;
+      notifyListeners();
+  }
+  _saveToPrefs() async {
+    await _initPrefs();
+    _pref.setDouble(keyFontSize, _fontSize);
+  }
+}
