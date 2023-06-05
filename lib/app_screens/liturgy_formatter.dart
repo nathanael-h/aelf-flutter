nathanael-h/aelf-flutter

commit ef0518a2de624fc07466b312176086058de3fea4

chg: settings_screen, remove old Region picker, replaced by a new one, bound to the liturgyState provider

lib/app_screens/settings_screen.dart
@@ -122,25 +122,6 @@ class _SettingsMenuState extends State<SettingsMenu> {
                         child: Divider(height: 1, color: Color.fromARGB(255, 94, 94, 94))
                       ),
                       Container(
-                        margin: EdgeInsets.fromLTRB(54, 0, 0, 16),
-                        child: RadioPickerSettingsTile(
-                            settingKey: keyPrefRegion,
-                            title: 'Régions',
-                            subtitle: _subtitle,
-                            values: {
-                                'afrique': 'Afrique',
-                                'belgique': 'Belgique',
-                                'canada': 'Canada',
-                                'france': 'France',
-                                'luxembourg': 'Luxembourg',
-                                'suisse' : 'Suisse',
-                                'romain' : 'Autre (Calendrier romain)',
-                            },
-                            defaultKey: 'romain',
-                            cancelCaption: 'Annuler',
-                        ),
-                      ),
-                      Container(
                         margin: EdgeInsets.fromLTRB(70, 20, 0, 32),
                         child: Text(
                           'Affichage',

commit 1eb0c15496bcca3a695d6475cdd846c55cc47792

fix: read Region from SharedPrefs. This was broken since last change in ef0518a2de624fc07466b312176086058de3fea4

lib/app_screens/liturgy_formatter.dart
@@ -484,6 +484,7 @@ class _LiturgyFormatterState extends State<LiturgyFormatter>
               body: LiturgyTabsView(tabsMap: loadingLiturgy()),
             );
           } else {
+            print("showing LiturgyTabsView: ${liturgyState.date} ${liturgyState.liturgyType.toString()} ${liturgyState.region}");
             return Scaffold(
             body: LiturgyTabsView(tabsMap: parseLiturgy(liturgyState.aelfJson)),
             );

lib/app_screens/settings_screen.dart
@@ -21,6 +21,7 @@ class _SettingsMenuState extends State<SettingsMenu> {
   }
   @override
   Widget build(BuildContext context) {
+    _region = context.watch<LiturgyState>().region;
     return Consumer<CurrentZoom>(
       builder: ((context, currentZoom, child) {
         return Scaffold(

lib/settings.dart
@@ -44,3 +44,9 @@ void setRegion(String newRegion) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   prefs.setString(keyPrefRegion, newRegion);
 }
+
+Future<String> getRegion() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String region = prefs.getString(keyPrefRegion) ?? 'romain';
+  return (region == '' ? '0' : region);
+}

lib/states/liturgyState.dart
@@ -35,6 +35,7 @@ class LiturgyState extends ChangeNotifier {
 
   LiturgyState() {
     print("LiturgyState init 1");
+    initRegion();
     updateLiturgy();
     autoSaveLiturgy();
   }
@@ -64,6 +65,15 @@ class LiturgyState extends ChangeNotifier {
       aelfJson = value;
       notifyListeners();
     });
+  }
+
+  void initRegion() async {
+    log('initRegion');
+    getRegion().then((savedRegion) {
+      region = savedRegion;
+    });
+    updateLiturgy();
+    notifyListeners();
   }
 
   Future<Map?> _getAELFLiturgy(String type, String date, String region) async {