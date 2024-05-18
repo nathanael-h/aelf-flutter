c550f993f30aed84ad443a31077ef773046bcea1
Nathanael <nathanael@hannebert.com>
2024-05-18 14:34:01 +0200

    add: PageState, which should handle all the logic regarding the Page view, and some settings like whereas some elements are visible or not. This would pave the way to have a new desktop/tablet view, with a left column navigation menu.

---
+++ lib/states/pageState.dart
@@ -0,0 +1,27 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class PageState extends ChangeNotifier {
+  String yolo = "yolo";
+  bool datePickerVisible = true;
+  bool searchVisible = false;
+  String title = "Titre";
+  var activeAppSection;
+
+  PageState() {
+    print("PageState init 1");
+  }
+
+  void function(String yolo) {
+    yolo = yolo + yolo;
+    notifyListeners();
+  }
+
+  void changeSearchButtonVisibility(bool Bool) {
+    log("changeSearchButtonVisibility to $Bool");
+    searchVisible = Bool;
+    notifyListeners();
+  }
+
+}
