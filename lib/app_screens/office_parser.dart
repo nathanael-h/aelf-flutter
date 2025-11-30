6989e0bd46de2a74232543a4295c0e85bc025599
Nathanaël <nathanael@isidorus.fr>
2025-11-30 19:47:05 +0100

    Bump to v1.12.0+6
    Starting from this commit, I will
    increase the builder number by
    one every time I build and submit
    an app version to a store. It seems
    to be requiered at least for macOS.

--- lib/app_screens/office_parser.dart
+++ lib/app_screens/office_parser.dart
@@ -247,7 +247,9 @@ class OfficeParser {
 
     // Récupérer l'antienne
     String subtitle =
-        office.containsKey("antienne_$nb") ? office["antienne_$nb"] ?? "" : "";
+        (office.containsKey("antienne_$nb") && office["antienne_$nb"] != null)
+            ? office["antienne_$nb"]
+            : "";
     subtitle = addAntienneBefore(subtitle);
 
     // Si pas d'antienne et psaume splitté, chercher l'antienne précédente
